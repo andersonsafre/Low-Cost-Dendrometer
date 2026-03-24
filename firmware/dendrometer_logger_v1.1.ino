f13cb85a1132e9fd75d19c7107d20aa8d45b3a6c
andersonsafre <andersonsafre@gmail.com>
2026-03-24 10:40:21 -0700

    Add files via upload

---
+++ firmware/dendrometer_logger_v1.1.ino
@@ -0,0 +1,81 @@
+#include <RTClib.h>
+#include "LowPower.h"
+#include <SD.h>
+
+int analogPin = A0;
+int sensorValue = 0;
+
+RTC_DS3231 rtc;
+File dataFile;
+
+void wakeup();
+
+void setup() {
+  Serial.begin(9600);
+  
+  rtc.begin();
+  SD.begin();
+  pinMode(3, INPUT_PULLUP);
+  
+  
+  ///rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  
+  
+  rtc.clearAlarm(1);
+  rtc.setAlarm1(
+    rtc.now() + TimeSpan(0, 0, 0, 2),
+    DS3231_A1_Second
+  );
+
+  
+}
+
+void loop() {
+  DateTime now = rtc.now();
+  sensorValue = analogRead(analogPin);
+  int distance = sensorValue;  
+  float v = sensorValue * (3.3 / 1023.0);
+  
+
+  Serial.print(now.timestamp(DateTime::TIMESTAMP_FULL));
+  Serial.print(",");
+  Serial.print(sensorValue);
+  Serial.print(",");
+  Serial.println(v,4);
+
+  
+  
+  dataFile = SD.open(("data.txt"),FILE_WRITE);   
+  if (dataFile){
+    dataFile.print(now.timestamp(DateTime::TIMESTAMP_FULL));
+    dataFile.print(", ");
+    dataFile.println(distance);
+  
+    dataFile.close();
+    }
+  
+  
+  // Set the alarm to trigger again in 10 seconds
+  rtc.setAlarm1(
+    rtc.now() + TimeSpan(0, 0, 0, 2),
+    DS3231_A1_Second
+  );
+  
+  
+  delay(1000);
+
+
+  rtc.clearAlarm(1);
+
+  
+  attachInterrupt(digitalPinToInterrupt(3), wakeup, FALLING);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+  
+}
+
+void wakeup() {
+  
+  detachInterrupt(0);  // Detach interrupt to avoid continuous wake-ups
+  // Clear the alarm flag
+  
+}
